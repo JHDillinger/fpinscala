6cd83ba2790b53356854f8eca755c112a5de6ba1
JHDillinger <notnecessary@local>
2018-05-07 19:56:10 +0200

    restructured the Applicative package

---
+++ exercises/src/main/scala/fpinscala/applicative/Monad.scala
@@ -0,0 +1,36 @@
+package fpinscala
+package applicative
+
+import monads.Functor
+import state._
+//import State._
+import StateUtil._ // defined at bottom of this file
+import monoids._
+import language.higherKinds
+import language.implicitConversions
+
+trait Monad[F[_]] extends Applicative[F] {
+  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B] = join(map(ma)(f))
+
+  def join[A](mma: F[F[A]]): F[A] = flatMap(mma)(ma => ma)
+
+  def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
+    a => flatMap(f(a))(g)
+
+  override def apply[A, B](mf: F[A => B])(ma: F[A]): F[B] =
+    flatMap(mf)(f => map(ma)(a => f(a)))
+}
+
+object Monad {
+  def eitherMonad[E]: Monad[({type f[x] = Either[E, x]})#f] = ???
+
+  def stateMonad[S] = new Monad[({type f[x] = State[S, x]})#f] {
+    def unit[A](a: => A): State[S, A] = State(s => (a, s))
+
+    override def flatMap[A, B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+      st flatMap f
+  }
+
+  def composeM[F[_], N[_]](implicit F: Monad[F], N: Monad[N], T: Traverse[N]):
+  Monad[({type f[x] = F[N[x]]})#f] = ???
+}
---
+++ exercises/src/main/scala/fpinscala/applicative/Traverse.scala
@@ -0,0 +1,59 @@
+package fpinscala
+package applicative
+
+import monads.Functor
+import state._
+//import State._
+import StateUtil._ // defined at bottom of this file
+import monoids._
+import language.higherKinds
+import language.implicitConversions
+
+trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
+  def traverse[G[_] : Applicative, A, B](fa: F[A])(f: A => G[B]): G[F[B]] =
+    sequence(map(fa)(f))
+
+  def sequence[G[_] : Applicative, A](fma: F[G[A]]): G[F[A]] =
+    traverse(fma)(ma => ma)
+
+  def map[A, B](fa: F[A])(f: A => B): F[B] = ???
+
+  import Applicative._
+
+  override def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
+    traverse[({type f[x] = Const[B, x]})#f, A, Nothing](
+      as)(f)(monoidApplicative(mb))
+
+  def traverseS[S, A, B](fa: F[A])(f: A => State[S, B]): State[S, F[B]] =
+    traverse[({type f[x] = State[S, x]})#f, A, B](fa)(f)(Monad.stateMonad)
+
+  def mapAccum[S, A, B](fa: F[A], s: S)(f: (A, S) => (B, S)): (F[B], S) =
+    traverseS(fa)((a: A) => for {
+      s1 <- get[S]
+      (b, s2) = f(a, s1)
+      _ <- set(s2)
+    } yield b).run(s)
+
+  override def toList[A](fa: F[A]): List[A] =
+    mapAccum(fa, List[A]())((a, s) => ((), a :: s))._2.reverse
+
+  def zipWithIndex[A](fa: F[A]): F[(A, Int)] =
+    mapAccum(fa, 0)((a, s) => ((a, s), s + 1))._1
+
+  def reverse[A](fa: F[A]): F[A] = ???
+
+  override def foldLeft[A, B](fa: F[A])(z: B)(f: (B, A) => B): B = ???
+
+  def fuse[G[_], H[_], A, B](fa: F[A])(f: A => G[B], g: A => H[B])
+                            (implicit G: Applicative[G], H: Applicative[H]): (G[F[B]], H[F[B]]) = ???
+
+  def compose[G[_]](implicit G: Traverse[G]): Traverse[({type f[x] = F[G[x]]})#f] = ???
+}
+
+object Traverse {
+  val listTraverse = ???
+
+  val optionTraverse = ???
+
+  val treeTraverse = ???
+}
