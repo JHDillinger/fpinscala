d143cae9f5dbe7068eb1de0ec5d2e01de50c4941
JHDillinger <notnecessary@local>
2018-05-10 22:08:33 +0200

    asdfadsfad

--- exercises/src/main/scala/fpinscala/iomonad/IO2c.scala
+++ exercises/src/main/scala/fpinscala/iomonad/IO2c.scala
@@ -20,21 +20,25 @@ object IO2c {
   sealed trait Async[A] { // will rename this type to `Async`
     def flatMap[B](f: A => Async[B]): Async[B] =
       FlatMap(this, f)
+
     def map[B](f: A => B): Async[B] =
       flatMap(f andThen (Return(_)))
   }
+
   case class Return[A](a: A) extends Async[A]
+
   case class Suspend[A](resume: Par[A]) extends Async[A] // notice this is a `Par`
-  case class FlatMap[A,B](sub: Async[A], k: A => Async[B]) extends Async[B]
+  case class FlatMap[A, B](sub: Async[A], k: A => Async[B]) extends Async[B]
 
   object Async extends Monad[Async] {
     def unit[A](a: => A): Async[A] = Return(a)
-    def flatMap[A,B](a: Async[A])(f: A => Async[B]): Async[B] = a flatMap f
+
+    def flatMap[A, B](a: Async[A])(f: A => Async[B]): Async[B] = a flatMap f
   }
 
   // return either a `Suspend`, a `Return`, or a right-associated `FlatMap`
   @annotation.tailrec def step[A](async: Async[A]): Async[A] = async match {
-    case FlatMap(FlatMap(x, f), g) => step(x.flatMap(a => f(a).flatMap(g)))
+    case FlatMap(FlatMap(x, f), g) => step(x flatMap (a => f(a) flatMap g))
     case FlatMap(Return(x), f) => step(f(x))
     case _ => async
   }
@@ -47,6 +51,7 @@ object IO2c {
       case _ => sys.error("Impossible, since `step` eliminates these cases")
     }
   }
+
   // The fact that `run` only uses the `unit` and `flatMap` functions of
   // `Par` is a clue that choosing `Par` was too specific of a choice,
   // this interpreter could be generalized to work with any monad.
